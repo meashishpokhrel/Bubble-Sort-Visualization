a9faea4717606e00da2a912d594fe024ebed1770
meashishpokhrel <meashishpokhrel@gmail.com>
2020-12-13 12:41:38 +0545

    Code Completed

--- src/bubble-sort-visualization.js
+++ src/bubble-sort-visualization.js
@@ -1,3 +1,6 @@
+//This code is written by Ashish Pokhrel 
+//Mini Project Computer Graphics
+//Bubble Sort Visualization
 
 
 let values = [];
@@ -6,16 +9,18 @@ let i = 0;
 let j = 0;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight); 
+  //Creating canvas of full window size taking its maximum width and Height.
+
   values = new Array(width);
   for (let i = 0; i < values.length; i++) {
     values[i] = random(height);
-    //values[i] = noise(i/100.0)*height;
+    
   }
 }
 
 function draw() {
-  background(0);
+  background("#0AD7D3");
 
   if (i < values.length) {
     for (let j = 0; j < values.length - i - 1; j++) {
@@ -26,13 +31,13 @@ function draw() {
       }
     }
   } else {
-    console.log("finished");
+    console.log("completed all");
     noLoop();
   }
   i++;
 
   for (let i = 0; i < values.length; i++) {
-    stroke(255);
+    stroke("#2112F3");
     line(i, height, i, height - values[i]);
   }
 }
@@ -42,3 +47,5 @@ function swap(arr, a, b) {
   arr[a] = arr[b];
   arr[b] = temp;
 }
+
+//Completed. For the output please run the code in live p5.js editor or open live server.
